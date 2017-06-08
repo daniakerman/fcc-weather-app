b739d73d976555e3cb635e02fc891f5ae146d105
Daniella <daniella.aum@gmail.com>
2017-06-08 18:31:27 +0300

    get location with freegeoip.net api

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-    console.log('hello');
+
+    /* get location */
+    $.getJSON("http://freegeoip.net/json/", function(data) {
+
+        var latitude = data.latitude;
+        var longitude = data.longitude;
+
+
+        $("<span>" + latitude + "</span>").appendTo('.location');
+        $("<span>" + longitude + "</span>").appendTo('.location');
+    });
 });
 
