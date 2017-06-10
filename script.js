09b8c76aa91d8d0327218e388b2e2a3f86a6595b
Daniella <daniella.aum@gmail.com>
2017-06-10 20:04:51 +0300

    append weather and location data to the elements

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 $(document).ready(function() {
 
     /* get location */
-    $.getJSON("http://freegeoip.net/json/", function(data) {
+    $.getJSON("http://freegeoip.net/json/", function(locationData) {
 
-        var latitude = data.latitude;
-        var longitude = data.longitude;
+        var latitude = locationData.latitude;
+        var longitude = locationData.longitude;
 
+        /*
+        console.log(locationData); */
 
-        $("<span>" + latitude + "</span>").appendTo('.location');
-        $("<span>" + longitude + "</span>").appendTo('.location');
+        /* load weather */
+        var urlWithLocation = "https://api.darksky.net/forecast/196806b6814ca53006c0347e2c052a5e/"
+            + latitude + "," + longitude;
 
-
-        var url = "https://api.darksky.net/forecast/196806b6814ca53006c0347e2c052a5e/" + latitude + "," + longitude;
         $.ajax({
-            url: url,
+            url: urlWithLocation,
             type: "GET",
             dataType: "jsonp",
-            success: function (data) {
-                console.log(data);
+            success: function (weatherData) {
+                // $('<span/>', {
+                //     'text': weatherData.currently.temperature
+                // }).appendTo('.temperature');
+                $("<span>" + weatherData.currently.temperature + "</span>").appendTo('.temperature');
+                $("<span>" + weatherData.currently.summary + "</span>").appendTo('.summary');
+                $("<span>" + weatherData.currently.icon + "</span>").appendTo('.icon');
+                $("<span>" + locationData.country_name + "</span>").appendTo('.location');
+                $("<span>" + locationData.city + "</span>").appendTo('.location');
+                $("<span>" + locationData.country_code + "</span>").appendTo('.location');
+
+
+                // console.log(weatherData);
             },
             xhrFields: {
                 withCredentials: false
             }
-        })
+        });
+
     });
 
 });
-
